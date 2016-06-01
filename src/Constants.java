f0d311e1e6b7ccd06f31a67a279869249bee43f6
thiagomohrs <thiagomohrs@gmail.com>
2016-06-01 12:46:01 -0300

    Iniciando cotrução lexica

---
+++ src/Constants.java
@@ -0,0 +1,53 @@
+public interface Constants extends ScannerConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_id = 2;
+    int t_num = 3;
+    int t_programa = 4;
+    int t_var = 5;
+    int t_caracter = 6;
+    int t_cadeia = 7;
+    int t_procedimento = 8;
+    int t_inicio = 9;
+    int t_fim = 10;
+    int t_inteiro = 11;
+    int t_booleano = 12;
+    int t_funcao = 13;
+    int t_se = 14;
+    int t_entao = 15;
+    int t_senao = 16;
+    int t_leia = 17;
+    int t_escreva = 18;
+    int t_ou = 19;
+    int t_e = 20;
+    int t_nao = 21;
+    int t_falso = 22;
+    int t_verdadeiro = 23;
+    int t_de = 24;
+    int t_faca = 25;
+    int t_real = 26;
+    int t_vetor = 27;
+    int t_enquanto = 28;
+    int t_TOKEN_29 = 29; //";"
+    int t_TOKEN_30 = 30; //","
+    int t_TOKEN_31 = 31; //"."
+    int t_TOKEN_32 = 32; //">"
+    int t_TOKEN_33 = 33; //"<"
+    int t_TOKEN_34 = 34; //"="
+    int t_TOKEN_35 = 35; //"("
+    int t_TOKEN_36 = 36; //")"
+    int t_TOKEN_37 = 37; //"["
+    int t_TOKEN_38 = 38; //"]"
+    int t_TOKEN_39 = 39; //"+"
+    int t_TOKEN_40 = 40; //"-"
+    int t_TOKEN_41 = 41; //"*"
+    int t_TOKEN_42 = 42; //"/"
+    int t_TOKEN_43 = 43; //":"
+    int t_TOKEN_44 = 44; //":="
+    int t_TOKEN_45 = 45; //".."
+    int t_TOKEN_46 = 46; //"<>"
+    int t_TOKEN_47 = 47; //"<="
+
+}
